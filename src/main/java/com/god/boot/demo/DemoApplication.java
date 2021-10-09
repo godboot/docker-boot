9123ceecac8f4119b568b0b0c2dfd07efdfb7b40
God Boot <goddoitnow@gmail.com>
2021-10-10 00:12:14 +0800

    Update DemoApplication.java

--- src/main/java/com/god/boot/demo/DemoApplication.java
+++ src/main/java/com/god/boot/demo/DemoApplication.java
@@ -2,6 +2,7 @@ package com.god.boot.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 
 /**
  * @author god boot
@@ -10,5 +11,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@GetMapping("test")
+	public String test(){
+		return "测试啊";
 	}
 }
