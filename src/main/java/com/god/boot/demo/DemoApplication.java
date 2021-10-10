ebc071f58d0b05151f068a9dd734fd7afbce7650
God Boot <goddoitnow@gmail.com>
2021-10-10 11:28:35 +0800

    init

--- src/main/java/com/god/boot/demo/DemoApplication.java
+++ src/main/java/com/god/boot/demo/DemoApplication.java
@@ -2,7 +2,6 @@ package com.god.boot.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
 
 /**
  * @author god boot
@@ -11,10 +10,5 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
-	}
-
-	@GetMapping("test")
-	public String test(){
-		return "测试啊";
 	}
 }
---
+++ src/main/java/com/god/boot/demo/controller/v1/DemoController.java
@@ -0,0 +1,21 @@
+package com.god.boot.demo.controller.v1;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author God Boot
+ * @version 1.0.0
+ * @className DemoController
+ * @description
+ * @createTime 2021年10月10日 11:16
+ */
+@RestController
+@RequestMapping("v1/demo")
+public class DemoController {
+    @GetMapping("test")
+    public String test(){
+        return "测试啊";
+    }
+}
